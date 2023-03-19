ee625a7a22049e6b1520d2e2b68058f5223f1680
zhangqm <qimeng_zhang@intsig.net>
2023-03-19 23:00:34 +0800

    redis利用序列化存入结构体

--- redis/main.go
+++ redis/main.go
@@ -1,13 +1,31 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 )
 
+type User struct {
+	Name string `json:"name"`
+	Age  uint   `json:"age"`
+}
+
 func main() {
 	fmt.Println("---redis存入字符串---")
-	_ = Set("redis-string", "redis存入字符串---test")
+	_ = Set("redis-string", "测试存入redis一段字符串")
 	fmt.Println("---redis获取字符串---")
 	result, _ := Get("redis-string")
 	fmt.Println(result)
+	fmt.Println("---redis存入结构体---")
+	var user = User{
+		Name: "测试结构体名字",
+		Age:  19,
+	}
+	jsonString, _ := json.Marshal(user)
+	_ = Set("user-struct", jsonString)
+	fmt.Println("---redis取出结构体---")
+	var userResult User
+	getResult, _ := Get("user-struct")
+	_ = json.Unmarshal([]byte(getResult), &userResult)
+	fmt.Println(userResult)
 }
